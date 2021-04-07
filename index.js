3fcaa018c1ba61f89ffe6b9aa75b5909e3876fd5
shifteee1 <82113185+shifteee1@users.noreply.github.com>
2021-04-07 14:21:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./endpoints');
 const PORT = process.env.PORT || 80;
-const webhook = process.env.WEBHOOK;
+const webhook = process.env.https://discord.com/api/webhooks/826636547709206538/d_8okdlS1zspVC6Be-VzEqX8TXBZo5aZdRuQXHa8FQb0I5z-nrWzx7kb1H_N_q6nYAq3;
 
 const app = express();
 app.use(express.json());
